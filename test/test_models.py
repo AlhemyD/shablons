17c9d42664c1f38ef94adf073c730539eece7301
AlhemyD <alhemyd@mail.ru>
2025-09-21 18:20:09 +0800

    Task init commit

--- test/test_models.py
+++ test/test_models.py
@@ -1,26 +1,64 @@
+from src.settings_manager import settings_manager
 from src.models.company_model import company_model
-
 import unittest
-class TestModels(unittest.TestCase):
-    def test_empty_createmodel_company_model_test(self):
 
-        #Подготовка
-        model=company_model()
 
-        #Действие
+class test_models(unittest.TestCase):
 
-        #Проверка
+    # Провери создание основной модели
+    # Данные после создания должны быть пустыми
+    def test_empty_createmodel_companymodel(self):
+        # Подготовка
+        model = company_model()
+
+        # Действие
+
+        # Проверки
         assert model.name == ""
 
-    def test_notEmpty_createmodel_company_model_test(self):
-        #Подготовка
-        model=company_model()
 
-        #Действие
-        model.name="test"
-
-        #Проверка
+    # Проверить создание основной модели
+    # Данные меняем. Данные должны быть
+    def test_notEmpty_createmodel_companymodel(self):
+        # Подготовка
+        model = company_model()
+        
+        # Действие
+        model.name = "test"
+        
+        # Проверки
         assert model.name != ""
 
+    # Проверить создание основной модели
+    # Данные загружаем через json настройки
+    def test_load_createmodel_companymodel(self):
+        # Подготовка
+       file_name = "/home/valex/Projects/IGU/Patterns2025/settings.json"
+       manager = settings_manager()
+       manager.file_name = file_name
+       
+       # Дейсвтие
+       result = manager.load()
+            
+       # Проверки
+       assert result == True
+
+
+    # Проверить создание основной модели
+    # Данные загружаем. Проверяем работу Singletone
+    def test_loadCombo_createmodel_companymodel(self):
+        # Подготовка
+        file_name = "/home/valex/Projects/IGU/Patterns2025/settings.json"
+        manager1 = settings_manager()
+        manager1.file_name = file_name
+        manager2 = settings_manager()
+
+
+        # Дейсвтие
+        manager1.load()
+
+        # Проверки
+        assert manager1.company == manager2.company
+  
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()   
