1fa1632faadba27bb8a83d928ab32e45640f3b2c
AlhemyD <alhemyd@mail.ru>
2025-10-02 09:55:11 +0800

    task 3 skip

--- src/models/settings_model.py
+++ src/models/settings_model.py
@@ -1,14 +1,19 @@
-from src.models.company_model import company_model
-from src.core.validator import validator
-class Settings:
+from Src.Models.company_model import company_model
+from Src.Core.validator import validator
+
+######################################
+# Модель настроек приложения
+class settings_model:
     __company: company_model = None
 
     # Текущая организация
     @property
     def company(self) -> company_model:
         return self.__company
-
+    
     @company.setter
     def company(self, value: company_model):
         validator.validate(value, company_model)
         self.__company = value
+
+
